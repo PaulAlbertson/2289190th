8c1b42cc0ace161915cad3a2152234d4d52a3115
Paul Albertson <palbertson@stompweb.com>
2017-05-22 16:00:14 -0700

    Initial commit

---
+++ app/scripts/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	'use strict';
+	$('.owl-carousel').owlCarousel({
+		autoPlay: 4000,
+		items:1,
+		itemsDesktop:[1199,3],
+		itemsDesktopSmall:[939,2],
+	});
+	$(document).foundation();
+});
